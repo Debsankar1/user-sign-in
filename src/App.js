b8eab4d6f3fc85785137bc9e5ca2807e5683f01a
Debsankar Mandal <67200982+Debsankar1@users.noreply.github.com>
2021-06-28 15:15:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Login from './components/login';
+// import LoginAuth from './components/loginAuth';
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      
+    </div>
+  );
+}
+
+export default App;
